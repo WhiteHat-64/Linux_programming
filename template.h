fc3ab63f4aff743a23c24b0991a10a044772c4d6
WhiteHat-64 <vinohari64@gmail.com>
2019-01-23 12:45:22 +0530

    Templates has to be followed to write Linux programming related files.

---
+++ template.h
@@ -0,0 +1,25 @@
+/***************************************/
+/* Author:                             */
+/* Date:                               */
+/* Filename:                           */
+/* Description:                        */
+/***************************************/
+
+#ifndef FILENAME_H
+#define FILENAME_H
+
+
+/* Included header files */
+
+
+/* Macro Definitions */
+
+
+/* user defined datatype definitions */
+
+
+/* function prototypes */
+
+
+
+#endif /* FILENAME_H */
